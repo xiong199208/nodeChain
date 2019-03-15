72c61a96e62fdc219ef773fa0bbcfa0754eb2117
huanglibo <313265006@qq.com>
2019-03-15 19:44:29 +0800

    格式化输出
    通过cli-table格式化控制台输出内容

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 const vorpal = require('vorpal')()
 const Blockchain = require('./blockchain')
 const blockchain = new Blockchain()
+const Table = require('cli-table')
+//格式化控制台输出
+function formatLatLog(data) {
+    if(!Array.isArray(data)) {
+        data = [data]
+    }
+
+    const one = data[0]
+    const head = Object.keys(one)
+
+    
+    const table = new Table({
+        head: head
+      , colWidths: new Array(head.length).fill(15)
+    });
+    const res = data.map(d=>{
+        return head.map(h=>d[h])
+    })
+    // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    table.push(...res);
+    console.log(res)
+    console.log(table.toString());
+}
 
 vorpal
     .command('mine','挖矿')
     .action(function(args,callback) {
         const block = blockchain.mine()
         if(block) {
-            console.log(block);
+            formatLatLog(block)
         }
         callback()
     })
 vorpal
     .command('chain','查看区块链')
     .action(function(args,callback) {
-        console.log(blockchain.blockchain);
+        formatLatLog(blockchain.blockchain)
         callback()
     })
 
